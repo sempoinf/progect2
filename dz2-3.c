4d3498c9a960b597afe64826fae565ed6feae692
sempoinf <91320209+sempoinf@users.noreply.github.com>
2021-10-15 10:03:04 +0300

    Create dz2-3.c

---
+++ dz2-3.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+	int N,f,s = 0;//число вершин
+	int i, j = 0;
+	int** graph;
+	
+	printf("Input the number of your knots: ");
+	scanf("%d", &N);
+	printf("%d", N);
+	
+	graph = (int**)malloc(N*N* sizeof(int));
+	if (!graph) {
+		printf("Allocation failure.");
+		exit(1);
+	}
+	printf("Input the graph: ");
+	
+	
+	getchar() = '-';
+
+
+	
+	
+	
+	
+
+}
