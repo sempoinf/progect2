b9c2e9127a0e8fab7b3ca2d2d3fc1df1b01e1625
sempoinf <91320209+sempoinf@users.noreply.github.com>
2021-10-15 16:19:18 +0300

    Update dz2-3.c

--- dz2-3.c
+++ dz2-3.c
@@ -3,28 +3,38 @@
 #include <stdio.h>
 #include<stdlib.h>
 int main(void) {
-	int N,f,s = 0;//число вершин
+	int N;//число вершин
 	int i, j = 0;
-	int** graph;
-	
+	int **graph;
 	printf("Input the number of your knots: ");
 	scanf("%d", &N);
-	printf("%d", N);
-	
-	graph = (int**)malloc(N*N* sizeof(int));
+	printf("%d\n", N);
+	graph = (int**)malloc(N*N * sizeof(int));
 	if (!graph) {
 		printf("Allocation failure.");
 		exit(1);
 	}
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++)
+			graph[i][j] = 0;
+	}
 	printf("Input the graph: ");
+	i = (getchar() - '0');
+		getchar(); //считываем -
+		j = (getchar() - '0');
+		graph[i][j]++;
 	
 	
-	getchar() = '-';
-
-
+		
+		
+		
+		
+		
+		for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++)
+			printf("%d", graph[i][j]);
+	}
 	
 	
 	
-	
-
 }
