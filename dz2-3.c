ed04921ae23bebd2eea695fcbc4a73ff9ab66a85
sempoinf <91320209+sempoinf@users.noreply.github.com>
2021-10-22 08:23:31 +0300

    Update dz2-3.c

--- dz2-3.c
+++ dz2-3.c
@@ -4,43 +4,56 @@
 #include<stdlib.h>
 
 int main(void) {
-	int N;//число вершин
+	int N,c;//число вершин
 	int i, j = 0;
-	int *graph[i];
-	
+	int **graph;//указатель на массив
+
 	printf("Input the number of your knots: ");
 	scanf("%d", &N);
-	printf("%d\n", N);
-	int* graph = (int*)malloc(N * sizeof(int*));//не выделяет память
-	
-	if (!graph) {
-		printf("Allocation failure.");
-		exit(1);
-	}
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++)
-			graph[i][j] = 0;
-	}
-	printf("Input the graph: ");
-	i = (getchar() - '0');
-		getchar(); //считываем -
-		j = (getchar() - '0');
-		graph[i-1][j-1]++;
-	
-		if (graph[i - 1][j - 1] > 1) printf("граф не дерево\n");
-		if (i == j) {
-			if(graph[i-1][j-1]>0) printf("граф не дерево\n");
-		}
-		
-		
-		
-		
-		
-		//for (i = 0; i < N; i++) {
-		//for (j = 0; j < N; j++)
-			//printf("%d", graph[i][j]);
+	//printf("%d\n", N);
+	//graph = (int*)malloc(N*N * sizeof(int));
+	int** graph = (int**)calloc(N, sizeof(int*));
+
+	//if (!graph) {
+		//printf("Allocation failure.");
+		//exit(1);
 	//}
-	
-	
-	
+	//for (i = 0; i < N; i++) {
+		//for (j = 0; j < N; j++)
+			//graph[i][j] = 0;
+	//}
+	printf("Input the graph: ");
+
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			i = (getchar() - '0');
+			getchar();
+			j = (getchar() - '0');
+			graph[i][j]++;
+				
+		}
+	}
+
+
+	//i = (getchar() - '0');
+	//getchar(); //считываем -
+	//j = (getchar() - '0');
+	//graph[i - 1][j - 1]++;
+
+	//if (graph[i - 1][j - 1] > 1) printf("граф не дерево\n");
+	//if (i == j) {
+		//if (graph[i - 1][j - 1] > 0) printf("граф не дерево\n");
+	//}
+
+
+
+
+
+	//for (i = 0; i < N; i++) {
+	//for (j = 0; j < N; j++)
+		//printf("%d", graph[i][j]);
+//}
+
+
+
 }
