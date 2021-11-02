3cb1a2b1faad3a6423df60634d341f40d5ab119f
sempoinf <91320209+sempoinf@users.noreply.github.com>
2021-11-02 19:11:59 +0300

    Update dz2-3.c

--- dz2-3.c
+++ dz2-3.c
@@ -4,12 +4,18 @@
 #include<stdlib.h>
 int N;
 int** graph;//tree
+int * b;
+
 
 int main(void) {
 	int c;//число вершин
 	int i, j = 0;
-
-
+	int check = 0;
+	int  tops, ribs;
+	int count = 0;
+	FILE* result;
+	
+	
 	printf("Input the number of your knots: ");
 	scanf("%d", &N);
 	printf("%d\n", N);
@@ -54,23 +60,88 @@ int main(void) {
 	fprintf(file, "}");
 	//close file
 	fclose(file);
+	
 
-
-
-	if (graph[i - 1][j - 1] > 1) printf("граф не дерево\n");
-	if (i == j) {
-		if (graph[i - 1][j - 1] > 0) printf("граф не дерево\n");
-	}
-
-
-
-
-
+	
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++)
 			printf("%d", graph[i][j]);
 		getchar();
 	}
+
+	
+	//Processing Graph
+
+	
+
+  // Петля
+
+	if (check == 0) {
+		for (int j = 0; j < N; j++) {
+			if (graph[j][j] == 1) {
+				check = 1;
+				break;
+			}
+		}
+	}
+	// =
+
+	if (check == 0) {
+		for (int i = 0; i < N; i++)
+			for (int j = 1; j < N; j++) {
+				if (((graph[j][i] == 1) && (graph[i][j] == 1)) || (graph[j][i] > 1) || ((graph[i][j] > 1))) {
+					check = 2;
+					break;
+				}
+			}
+	}
+
+	//Связанность
+
+	b = (int*)malloc(N * sizeof(int));
+	for (int i = 0; i < N; i++) {
+		b[i] = 0;
+	}
+	for (int i = 0; i <= N; i++) {
+		b[graph[i]] ++;
+	}
+	for (int i = 0; i <= N; i++) {
+		if (b[i] > 0)
+			tops++;
+	}
+
+	//printf("ribs = %d, tops = %d", ribs, tops);
+
+
+	//if (check == 0) {
+		//if ((ribs) >= (tops - 1)) {
+			//check = 0;
+		//}
+		//else {
+		//	check = 3;
+		//}
+	//}
+
+	//Цикл
+
+
+	//if (check == 0) {
+		//if ((tops - ribs) != 1) {
+		//	check = 4;
+		//}
+	//}
+
+	result = fopen("result.txt", "w");
+	fprintf(result, "Result:\n");
+	if (check != 0) {
+		fprintf(result, "This Graph is not a TREE\n");
+		printf("This Graph is not a TREE\n");
+	}
+	if (check == 0) {
+		printf("This Graph is a TREE\n");
+		fprintf(result, "This Graph is a TREE\n");
+	}
+	fclose(result);
 
 
 
